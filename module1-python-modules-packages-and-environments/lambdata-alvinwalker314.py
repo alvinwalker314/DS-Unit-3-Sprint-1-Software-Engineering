e40ca08df0a7e41459debc4ef3f5da590e8609c6
alvinwalker314 <46687252+alvinwalker314@users.noreply.github.com>
2019-07-08 18:09:29 -0400

    Add files via upload

---
+++ module1-python-modules-packages-and-environments/lambdata-alvinwalker314.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+def split_date(df, date_column):
+	years= [data.split('/')[0] for data in df[date_column]]
+	months= [data.split('/')[1] for data in df[date_column]]
+	days= [data.split('/')[2] for data in df[date_column]]
+	df['year']= years
+	df['month']= months
+	df['day']= days
+	return df
+
+def list_series_dataframe(data, column_name):
+  series= pd.Series(data) #turn list into a pandas series
+  df= pd.DataFrame(columns=column_name, data=data) #turn list into a pandas dataframe
+  return df #returning new dataframe
